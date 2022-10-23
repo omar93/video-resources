c6d178e4b682d968ef69e96a727edba6aa4fabe3
Omar Alhamad <omar93@hotmail.se>
2022-10-23 13:15:14 +0200

    added all old svelte files to sveltekit, need alot of recatoring and changing for it to work

---
+++ src/lib/videolist.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Video from "./video.svelte";
+    import { list } from "../stores/videos.js";
+    
+    const handleRemove = event => {
+        const id = event.detail.id
+        $list = $list.filter(video => video.id !== id);
+    }
+
+</script>
+
+<ul id="videoList--container">
+    {#each $list as video}
+        <Video {...video} on:remove={handleRemove}/>
+    {/each}
+</ul>
+
+<style>
+    #videoList--container {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 0.2fr) );
+        gap: 20px;
+        padding-left: 20px;
+        padding-right: 20px;
+        list-style-type: none;
+    }
+
+</style>
