17775c427eb6ff819c6f3aaf27bdaf62b3788488
Omar Alhamad <omar93@hotmail.se>
2022-10-23 13:18:38 +0200

    setting up skeleton for project

---
+++ src/lib/startpage.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Videolist from "./lib/videolist.svelte";
+    import { list } from "./stores/videos.js";
+    import { v4 as uuidv4 } from 'uuid';
+
+    let inputValue = ''
+    
+    const handleNewVideoSubmit = async () => {
+        let youtubeID = inputValue.split('v=')[1]
+        console.log(inputValue);
+        let imageHandler = `https://i.ytimg.com/vi/${youtubeID}/hqdefault.jpg`
+        console.log(imageHandler);
+        $list = [...$list, {id: uuidv4(), imgUrl: imageHandler, videoUrl: inputValue}]
+    }
+</script>
+
+<div id="main--container">
+    <form id="link--form" on:submit|preventDefault={handleNewVideoSubmit}>
+        <input type="text" name="link" placeholder="Youtube Link" bind:value={inputValue}/>
+    </form>
+<hr>
+    <div id="video--container">
+        <Videolist/>
+    </div>
+</div>
+
+<style>
+    #main--container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        /*justify-content: center;*/
+        height: 100vh;
+        width: 100vw;
+    }
+
+    form {
+        margin-top: 20px;
+    }
+
+    #video--container {
+        margin-top: 50px;
+        height: 90%;
+        width: 100%;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+</nav>
+  
+<slot></slot>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+        background: #eee;
+    }
+    nav a {
+        color: #333;
+        text-decoration: none;
+        font-weight: bold;
+    }
+</style>
