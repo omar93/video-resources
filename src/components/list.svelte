dbb231d5228a56e41c2f1f8fd0a7dad6e0665fce
Omar Alhamad <omar93@hotmail.se>
2022-10-27 16:40:53 +0200

    added list style

--- src/components/list.svelte
+++ src/components/list.svelte
@@ -1,3 +1,14 @@
 <ul>
     <li>items</li>
 </ul>
+
+<style>
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+    li {
+        padding: 1rem;
+        border-bottom: 1px solid #eee;
+    }
+</style>
