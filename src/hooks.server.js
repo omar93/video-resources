dee4bf93d0639c11f2668cf90e084fc277f9adc1
Omar Alhamad <omar93@hotmail.se>
2022-11-21 16:19:12 +0100

    changed unsecure to secure in cookies

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -14,7 +14,7 @@ export async function handle({ event, resolve }) {
     const response = await resolve(event);
 
     // send back the default 'pb_auth' cookie to the client with the latest store state
-    response.headers.set('set-cookie', event.locals.pocketbase.authStore.exportToCookie({secure:false}));
+    response.headers.set('set-cookie', event.locals.pocketbase.authStore.exportToCookie({secure:true}));
 
     return response;
 }
